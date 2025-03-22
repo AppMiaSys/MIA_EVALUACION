19235916ae3bc936c3950e45846e7626ffa3b885
AppMiaSys <jose.rojas@grupohardtech.com>
2025-03-22 00:16:38 -0500

    Update Configuracion.jsx

--- frontend/src/pages/Configuracion.jsx
+++ frontend/src/pages/Configuracion.jsx
@@ -200,9 +200,13 @@ const Configuracion = () => {
         </div>
 
         <ul className="list-disc list-inside bg-white p-3 rounded shadow">
-          {Array.isArray(categorias) && categorias.map((c) => (
-            <li key={c.id}>{c.nombre}</li>
-          ))}
+          {Array.isArray(categorias) &&
+  categorias.map((cat) => (
+    <option key={cat.id} value={cat.id}>
+      {cat.nombre}
+    </option>
+  ))}
+
         </ul>
       </section>
 
