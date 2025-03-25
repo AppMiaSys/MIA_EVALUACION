9f8dd54008c4dc4b6409648bf950fb7931f2c827
AppMiaSys <jose.rojas@grupohardtech.com>
2025-03-25 08:57:34 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,32 +1,32 @@
-// ✅ src/services/api.js
+// ✅ src/services/api.js actualizado
 
 import axios from "axios";
 
-const API = "https://mia-backend.onrender.com/api"; // Cambiar si estás en local
+const API = "https://mia-backend.onrender.com/api";
 
-export const getEmpleados = async () => axios.get(`${API}/empleados`);
-export const addEmpleado = async (data) => axios.post(`${API}/empleados`, data);
-export const updateEmpleado = async (data) => axios.put(`${API}/empleados`, data);
+export const getEmpleados = () => axios.get(`${API}/empleados`);
+export const addEmpleado = (data) => axios.post(`${API}/empleados`, data);
+export const updateEmpleado = (data) => axios.put(`${API}/empleados`, data);
 
-export const getCategorias = async () => axios.get(`${API}/categorias`);
-export const addCategoria = async (data) => axios.post(`${API}/categorias`, data);
+export const getCategorias = () => axios.get(`${API}/categorias`);
+export const addCategoria = (data) => axios.post(`${API}/categorias`, data);
+export const updateCategoria = (data) => axios.put(`${API}/categorias`, data);
 
-export const getPreguntas = async () => axios.get(`${API}/preguntas`);
-export const addPregunta = async (data) => axios.post(`${API}/preguntas`, data);
-export const updatePregunta = async (data) => axios.put(`${API}/preguntas`, data);
+export const getPreguntas = () => axios.get(`${API}/preguntas`);
+export const addPregunta = (data) => axios.post(`${API}/preguntas`, data);
+export const updatePregunta = (data) => axios.put(`${API}/preguntas`, data);
 
-export const getNiveles = async () => axios.get(`${API}/niveles`);
-export const addNivel = async (data) => axios.post(`${API}/niveles`, data);
-export const updateNivel = async (data) => axios.put(`${API}/niveles`, data);
+export const getNiveles = () => axios.get(`${API}/niveles`);
+export const addNivel = (data) => axios.post(`${API}/niveles`, data);
+export const updateNivel = (data) => axios.put(`${API}/niveles`, data);
 
-export const getAsignaciones = async (dni) => axios.get(`${API}/asignaciones/${dni}`);
-export const asignarEvaluados = async (evaluador, evaluados) =>
-  axios.post(`${API}/asignaciones`, { evaluador, evaluados });
+export const getAsignaciones = (dni) => axios.get(`${API}/asignaciones/${dni}`);
+export const asignarEvaluados = (data) => axios.post(`${API}/asignaciones`, data);
 
-export const enviarEvaluacion = async (data) => axios.post(`${API}/evaluacion`, data);
-export const getResultadosPorEmpleado = async (dni) => axios.get(`${API}/resultados/${dni}`);
+export const enviarEvaluacion = (data) => axios.post(`${API}/evaluaciones`, data);
+export const getResultadosPorEmpleado = (dni) => axios.get(`${API}/resultados/${dni}`);
 
-// 🔐 Niveles de acceso
-export const getNivelesAcceso = async () => axios.get(`${API}/niveles-acceso`);
-export const addNivelAcceso = async (data) => axios.post(`${API}/niveles-acceso`, data);
-export const updateNivelAcceso = async (data) => axios.put(`${API}/niveles-acceso`, data);
+export const getNivelesAcceso = () => axios.get(`${API}/niveles-acceso`);
+export const addNivelAcceso = (data) => axios.post(`${API}/niveles-acceso`, data);
+export const updateNivelAcceso = (data) => axios.put(`${API}/niveles-acceso`, data);
+
